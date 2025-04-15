fc45607fdfde764b8a26b492d4da71a93a13e600
smartflomeet <atul.titare@tatatel.co.in>
2025-04-15 14:58:20 +0530

    Update the package

---
+++ Package.swift
@@ -0,0 +1,34 @@
+//
+//  Untitled.swift
+//  SampleApp_VCX_SDK
+//
+//  Created by jaykumar on 15/04/25.
+//
+
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "EnxRTCiOS",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "EnxRTCiOS",
+            targets: ["EnxRTCiOS"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "EnablexWebRTC",
+            url: "https://www.enablex.io/downloads/EnablexWebRTC.xcframework_1.0.4.zip",
+            checksum: "6ef4e8c6f78eddbc9d4f478ea14fb111cf58199a976b15aff37779ae0fe04b6c"
+        ),
+        .binaryTarget(
+            name: "EnxRTCiOS",
+            url: "https://developer.enablex.io/downloads/iOS_SDK_3.0.19/EnxRTCiOS.xcframework_3.0.19.zip",
+            checksum: "fe1b00d967b71ca67901ed0ea40bfd30054cfae2222a1adb450a280abd2b738c"
+        )
+    ]
+)

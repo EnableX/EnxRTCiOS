177d112e72368420e08edb2986a17138f1d55378
jaykumarvcx <jayprakash.k@vcloudx.com>
2025-05-19 16:53:38 +0530

    Updated sdk with Vresion 3.0.20

--- Package.swift
+++ Package.swift
@@ -14,12 +14,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EnxRTCiOS",
-            url: "https://developer.enablex.io/downloads/iOS_SDK_3.0.19/EnxRTCiOS.xcframework_3.0.19.zip",
-            checksum: "cbc9ef5eb8ebd26de8e95fb36125a703fde4786146ae189c0d4dbd0a934278f1"
+            url: "https://developer.enablex.io/downloads/iOS_SDK_3.0.20/EnxRTCiOS.xcframework_3.0.20.zip",
+            checksum: "e246b8f951cf222829d1b5ef7c615d4a2e37844983d5f1c06533a8219252e1e6"
         ),
         .binaryTarget(
             name: "EnablexWebRTC",
-            url: "https://www.enablex.io/downloads/EnablexWebRTC.xcframework_1.0.4.zip",
+            url: "https://developer.enablex.io/downloads/EnablexWebRTC.xcframework_1.0.4.zip",
             checksum: "6ef4e8c6f78eddbc9d4f478ea14fb111cf58199a976b15aff37779ae0fe04b6c"
         )
     ]

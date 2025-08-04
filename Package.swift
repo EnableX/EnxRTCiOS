8b6d8fba4916f3809a8991b0bede4465545cfb85
jaykumarvcx <jayprakash.k@vcloudx.com>
2025-08-04 12:14:57 +0530

    Update release version 3.1.3

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@ import PackageDescription
 let package = Package(
     name: "EnxRTCiOS",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v15)
     ],
     products: [
         .library(name: "EnxRTCiOS", targets: ["EnxRTCiOS", "EnablexWebRTCWrapper"])
     ],
     dependencies: [
-        .package(url: "https://github.com/EnableX/EnablexWebRTC.git", from: "2.0.1")
+        .package(url: "https://github.com/EnableX/EnablexWebRTC.git", from: "2.0.2")
     ],
     targets: [
         .binaryTarget(
             name: "EnxRTCiOS",
-            url: "https://developer.enablex.io/downloads/iOS_SDK_3.1.2/EnxRTCiOS.xcframework_3.1.2.zip",
-            checksum: "522accdcd09fd54a42174a5e7fa5e5dce676d884d44097cfd176672c4a1487e7"
+            url: "https://developer.enablex.io/downloads/iOS_SDK_3.1.3/EnxRTCiOS.xcframework_3.1.3.zip",
+            checksum: "3eb8da549ec613ae8d7eecd3ffd56e792542ae0519ede755cd188d55799addd1"
         ),
         .target(
             name: "EnablexWebRTCWrapper",
